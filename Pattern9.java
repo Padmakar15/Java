50c1e75db4c8ac925be297a93402aa49546bd1ba
Padmakar Kendre <50291503+Padmakar15@users.noreply.github.com>
2021-03-29 00:52:59 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,35 @@
+/*
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+ */
+import java.util.Scanner;
+class Pattern9{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no of rows");
+        int rows=sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int sp=rows-i-1;sp>0;sp--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<rows;i++){
+            for(int sp=i;sp>0;sp--){
+                System.out.print(" ");
+            }
+            for(int j=rows-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
